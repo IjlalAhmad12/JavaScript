86854c528bf4d23d0f63c60cfb5eaa6ff458c39b
IjlalAhmad12 <141921703+IjlalAhmad12@users.noreply.github.com>
2024-03-30 18:12:54 +0000

    variables

---
+++ 01_basics/variableconst.js
@@ -0,0 +1,10 @@
+const name = "Ijlal"
+let age = 25 
+var occupation = "Engineer"
+Account = "ICICI Bank"
+let maritalStatus;
+
+age = 28
+occupation = null;
+console.table([name , age , occupation , Account , maritalStatus])
+console.log(typeof maritalStatus)
